8bfaf1aaa58d687dcae51e46522dc276a15f1015
Raja Vignesh <r.vignesh@cheil.com>
2024-12-16 19:49:42 +0530

    ui modify on signup screen

--- app/login/signup/page.js
+++ app/login/signup/page.js
@@ -23,7 +23,6 @@ import MultiSelectRole from "@/app/src/components/multiSelectDropdown/MultiSelec
 import toast, { Toaster } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
-
 function Signup() {
   const [selectedSubsidiary, setSelectedSubsidiary] = useState([]);
   const [selectedRole, setSelectedRole] = useState([]);
@@ -36,12 +35,20 @@ function Signup() {
     referredBy: "",
     passwordConfirm: "",
     mobilenumber: "",
-    dob:"",
-    gender: '',
-    language: '',
+    dob: "",
+    gender: "",
+    language: "",
   });
-  const { username, password, passwordConfirm, mobilenumber, referredBy,dob,language,gender } =
-    inputFields;
+  const {
+    username,
+    password,
+    passwordConfirm,
+    mobilenumber,
+    referredBy,
+    dob,
+    language,
+    gender,
+  } = inputFields;
   const [token, setToken] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [showPassword, setShowPassword] = useState(false);
@@ -101,33 +108,34 @@ function Signup() {
 
     try {
       setLoading(true);
-      const requestData = { user: {
-        email:username,
-        password,
-        passwordConfirm,
-        mobileNumber:mobilenumber,
-        referredBy,
-        roles: selectedRole.map(item => ({
-          recordId: item.recordId
-      })),
-        dateOfBirth:dob,
-        gender:gender,
-        language:language,
-      }};
+      const requestData = {
+        user: {
+          email: username,
+          password,
+          passwordConfirm,
+          mobileNumber: mobilenumber,
+          referredBy,
+          roles: selectedRole.map((item) => ({
+            recordId: item.recordId,
+          })),
+          dateOfBirth: dob,
+          gender: gender,
+          language: language,
+        },
+      };
       console.log(requestData, "req body");
 
       const response = await axios.post(api + "/register", requestData);
       console.log(response.data, "registration response");
-      if(response.data.success == true){
-        toast.success(`User Register Successfully`,{className:"text-sm"});
+      if (response.data.success == true) {
+        toast.success(`User Register Successfully`, { className: "text-sm" });
         setTimeout(() => {
-        router.push("/login");
-  
-      }, 2000);
-      }else{
-      toast.error(`User already Registred`,{className:"text-sm"});
+          router.push("/login");
+        }, 2000);
+      } else {
+        toast.error(`User already Registred`, { className: "text-sm" });
       }
-      
+
       setLoading(false);
     } catch (error) {
       console.error(error, "registration error");
@@ -143,15 +151,12 @@ function Signup() {
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
-console.log(selectedRole,"selected selectedRole");
+  console.log(selectedRole, "selected selectedRole");
   return (
     <div className="flex flex-col min-h-screen justify-start lg:justify-center items-center p-1 pt-5 lg:pt-2 md:p-5">
       <Toaster />
       <div className="mb-5 flex justify-center w-48 h-20">
-        <Image priority
-          src={logo}
-          alt="Zero-in Logo"
-        />
+        <Image priority src={logo} alt="Zero-in Logo" />
       </div>
 
       <div className="flex flex-col gap-3 w-[100%] md:w-[90%] lg:w-[40%] justify-center items-center rounded-lg p-8">
@@ -172,7 +177,10 @@ console.log(selectedRole,"selected selectedRole");
 
         {/* Password Field with Visibility Toggle */}
         <div className="flex flex-col md:flex-row gap-3 md:gap-20 w-[100%]">
-          <FormControl className="flex flex-col w-full md:w-[45%]" variant="standard">
+          <FormControl
+            className="flex flex-col w-full md:w-[45%]"
+            variant="standard"
+          >
             <InputLabel htmlFor="password">Password</InputLabel>
             <Input
               id="password"
@@ -196,7 +204,10 @@ console.log(selectedRole,"selected selectedRole");
           </FormControl>
 
           {/* Confirm Password Field with Visibility Toggle */}
-          <FormControl className="flex flex-col w-full md:w-[45%]" variant="standard">
+          <FormControl
+            className="flex flex-col w-full md:w-[45%]"
+            variant="standard"
+          >
             <InputLabel htmlFor="confirm-password">Confirm Password</InputLabel>
             <Input
               id="confirm-password"
@@ -224,7 +235,8 @@ console.log(selectedRole,"selected selectedRole");
         </div>
 
         <div className="flex flex-col w-full ">
-          <TextField type="number"
+          <TextField
+            type="number"
             value={mobilenumber}
             onChange={(e) =>
               setInputFields({ ...inputFields, mobilenumber: e.target.value })
@@ -235,7 +247,8 @@ console.log(selectedRole,"selected selectedRole");
           />
         </div>
         <div className="flex flex-col w-full mt-2">
-          <TextField type="date"
+          <TextField
+            type="date"
             value={dob}
             onChange={(e) =>
               setInputFields({ ...inputFields, dob: e.target.value })
@@ -245,40 +258,44 @@ console.log(selectedRole,"selected selectedRole");
           />
         </div>
         <div className="flex flex-col w-full mt-2">
-        <TextField
-          select
-          label="Gender"
-          value={gender}
-          onChange={(e) => setInputFields({ ...inputFields, gender: e.target.value })}
-          variant="standard"
-          className="text-xs w-[100%]"
-        >
-          <MenuItem value="Male">Male</MenuItem>
-          <MenuItem value="Female">Female</MenuItem>
-          <MenuItem value="Other">Other</MenuItem>
-        </TextField>
-      </div>
-      <div className="flex flex-col w-full mt-2">
-        <TextField
-          select
-          label="Select Language"
-          value={inputFields.language}
-          onChange={(e) => setInputFields({ ...inputFields, language: e.target.value })}
-          variant="standard"
-          className="text-xs w-[100%]"
-        >
-          <MenuItem value="English">English</MenuItem>
-          <MenuItem value="Spanish">Spanish</MenuItem>
-          <MenuItem value="French">French</MenuItem>
-          <MenuItem value="German">German</MenuItem>
-          <MenuItem value="Mandarin">Mandarin</MenuItem>
-        </TextField>
-      </div>
+          <TextField
+            select
+            label="Gender"
+            value={gender}
+            onChange={(e) =>
+              setInputFields({ ...inputFields, gender: e.target.value })
+            }
+            variant="standard"
+            className="text-xs w-[100%]"
+          >
+            <MenuItem value="Male">Male</MenuItem>
+            <MenuItem value="Female">Female</MenuItem>
+            <MenuItem value="Other">Other</MenuItem>
+          </TextField>
+        </div>
+        <div className="flex flex-col w-full mt-2">
+          <TextField
+            select
+            label="Select Language"
+            value={inputFields.language}
+            onChange={(e) =>
+              setInputFields({ ...inputFields, language: e.target.value })
+            }
+            variant="standard"
+            className="text-xs w-[100%]"
+          >
+            <MenuItem value="English">English</MenuItem>
+            <MenuItem value="Spanish">Spanish</MenuItem>
+            <MenuItem value="French">French</MenuItem>
+            <MenuItem value="German">German</MenuItem>
+            <MenuItem value="Mandarin">Mandarin</MenuItem>
+          </TextField>
+        </div>
 
         <div className="flex flex-col w-full gap-3 ">
           <MultiSelectRole
-          initialload={initialload}
-          selectedRoles={selectedRole}
+            initialload={initialload}
+            selectedRoles={selectedRole}
             setSelectedRoles={setSelectedRole}
           />
         </div>
@@ -296,13 +313,13 @@ console.log(selectedRole,"selected selectedRole");
         </div>
 
         <div className="text-xs text-center mt-2">
-          (The referrer is your department head. He/she will receive the confirmation
-          email and approve your access.)
+          (The referrer is your department head. He/she will receive the
+          confirmation email and approve your access.)
         </div>
 
         <div className="text-xs text-center mt-2">
-          By clicking Submit, I agree to the Terms and Conditions of Zero-in Platform
-          usage and the Privacy Statement.
+          By clicking Submit, I agree to the Terms and Conditions of Zero-in
+          Platform usage and the Privacy Statement.
         </div>
 
         <div className="flex flex-col w-full mt-4">
